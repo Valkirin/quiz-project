ba4be3a429d3ef41288ded179a74a0aa8180ee18
Valkirin <valkirinn@gmail.com>
2020-09-08 14:08:25 +0300

    2-Create layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from './hoc/Layout/Layout';
 
 function App() {
-  return <div className='App'>Hello React</div>;
+  return (
+    <Layout>
+      <div style={{ width: 400, border: '1px solid black' }}>
+        <h1>Layout works</h1>
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
