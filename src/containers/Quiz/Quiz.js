3c2e428eda527959c002b5d1c404bb02719c7805
Valkirin <valkirinn@gmail.com>
2020-09-09 15:14:19 +0300

    stylization and relations

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,15 +5,24 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 export class Quiz extends Component {
   //dynamical content
   state = {
-    quiz: [],
+    quiz: [
+      {
+        answers: [
+          { text: 'Answer 1' },
+          { text: 'Answer 2' },
+          { text: 'Answer 3' },
+          { text: 'Answer 4' },
+        ],
+      },
+    ],
   };
 
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz />
+          <h1>PAEI test</h1>
+          <ActiveQuiz answers={this.state.quiz[0].answers} />
         </div>
       </div>
     );
