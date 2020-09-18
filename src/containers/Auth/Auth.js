37bf46caca197449b127d9dad578be7f8bbd8336
Valkirin <valkirinn@gmail.com>
2020-09-19 01:01:35 +0300

    big routing add

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Auth extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Authenticate</h1>
+      </div>
+    );
+  }
+}
