9a7c695399da2db7db4f094a012fed2c578d8a7c
Valkirin <valkirinn@gmail.com>
2020-09-20 23:45:27 +0300

    authorization page styled

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import classes from './Auth.css';
+import Button from '../../components/UI/Button/Button';
 
 export default class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Authenticate</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Authenticate</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type='text' />
+            <input type='text' />
+
+            <Button type={'success'} onClick={this.loginHandler}>
+              Sign In
+            </Button>
+
+            <Button type={'primary'} onClick={this.registerHandler}>
+              Sign Up
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
