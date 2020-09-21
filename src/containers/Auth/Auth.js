08201cfb10d0cde96d3d78a9d593ec3289f898f9
Valkirin <valkirinn@gmail.com>
2020-09-21 12:53:23 +0300

    validation finished

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,8 @@ import is from 'is_js';
 
 export default class Auth extends Component {
   state = {
+    isFormValid: false,
+
     formControls: {
       email: {
         value: '',
@@ -74,8 +76,15 @@ export default class Auth extends Component {
 
     formControls[controlName] = control;
 
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+
     this.setState({
       formControls,
+      isFormValid,
     });
   };
 
@@ -107,11 +116,19 @@ export default class Auth extends Component {
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
             {this.renderInputs()}
 
-            <Button type={'success'} onClick={this.loginHandler}>
+            <Button
+              type={'success'}
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               Sign In
             </Button>
 
-            <Button type={'primary'} onClick={this.registerHandler}>
+            <Button
+              type={'primary'}
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
               Sign Up
             </Button>
           </form>
