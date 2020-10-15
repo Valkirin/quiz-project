f33ad7341aadcdca2dbccf40d743d638cbef2fa0
Valkirin <valkirinn@gmail.com>
2020-10-15 20:08:08 +0300

    authentication is refactored

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import function from '../../../../React-Redux-Boilerplate/dev/js/reducers/reducer-active-user';
+import { Redirect } from 'react-router-dom';
 import { logout } from '../../store/actions/auth';
 
 class Logout extends Component {
-    componentDidMount() {
-        this.props.logout()
-    }
-    
-    render() {
-        return (
-            <div>
-                <Redirect to={'/'}/>
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <div>
+        <Redirect to={'/'} />
+      </div>
+    );
+  }
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        logout: () => dispatch(logout())
-    }
+  return {
+    logout: () => dispatch(logout()),
+  };
 }
 
-export default connect(null, mapDispatchToProps)(Logout)
+export default connect(null, mapDispatchToProps)(Logout);
