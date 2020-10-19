791dddc2697eb56480c951adf7e49da1eacd6e44
Valkirin <valkirinn@gmail.com>
2020-10-19 14:53:01 +0300

    styles added

--- src/components/UI/Alert/Alert.js
+++ src/components/UI/Alert/Alert.js
@@ -1,7 +1,43 @@
-import React from 'react';
-import classes from './Alert.css';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-const Alert = (props) => {
-  return <div className={classes.Alert}>Error</div>;
+const Alert = () => {
+  const { alerts } = useSelector((state) => state.notifications);
+  const [alert, setAlert] = useState({ type: '', message: '' });
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (alerts.length > 0) {
+      setAlert(alerts[alerts.length - 1]);
+      setShow(true);
+      setTimeout(() => {
+        setShow(false);
+      }, 3000);
+    }
+  }, [alerts]);
+
+  const onClose = () => {
+    setShow(false);
+  };
+
+  const color = alert.type === 'success' ? 'teal' : 'tomato';
+  const iconUrl = alert.type === 'success' ? successIcon : errorIcon;
+
+  return show ? (
+    <div className={`${alert.type || 'error'}`}>
+      <Content>
+        <Icon icon={iconUrl} color={color} size='20px' />
+        <Message>{alert.message || ''}</Message>
+      </Content>
+      <Icon
+        icon={closeIcon}
+        color={color}
+        size='24px'
+        onClick={onClose}
+        style={{ cursor: 'pointer' }}
+      />
+    </div>
+  ) : null;
 };
+
 export default Alert;
--- src/store/reducers/alert.js
+++ src/store/reducers/alert.js
@@ -1,4 +1,4 @@
-import { ALERT_ERROR } from '../actions/actionTypes';
+import { ALERT_ERROR, ALERT_SUCCESS } from '../actions/actionTypes';
 
 const initialState = {
   alerts: [],
@@ -6,11 +6,18 @@ const initialState = {
 
 export default function alertReducer(state = initialState, action) {
   switch (action.type) {
-    case ALERT_ERROR:
+    case ALERT_SUCCESS:
       return {
         ...state,
         alerts: [action.payload.message],
       };
+
+    case ALERT_ERROR:
+      return {
+        ...state,
+        alerts: [action.error.message],
+      };
+
     default:
       return state;
   }
